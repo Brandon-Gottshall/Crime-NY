a6abcac71ccd82bc24707e6325914cf2dc368dbb
Brandon Gottshall <Brandon@gottshall.dev>
2020-01-26 23:18:14 -0500

    Added Slider Component.

--- src/Components/DataHandler.js
+++ src/Components/DataHandler.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Map from './Map.js'
+import Slider from './Slider.js'
 
 const DataHandler = props => {
     return (
         <div style={{backgroundColor: 'red'}}>
             <p>This is the datahandler component.</p>
             <Map />
+            <Slider/>
         </div>
     )
 }
