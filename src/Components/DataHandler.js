7174c324792e8c2d119e4520b8027aa2de2c9a6d
Brandon @ Moons Out <brandon@moonsoutmedia.com>
2026-05-09 18:04:53 -0400

    Fix CI build lint warnings

--- src/Components/DataHandler.js
+++ src/Components/DataHandler.js
@@ -15,46 +15,47 @@ import Violation from '../Images/Violation.png'
 const DataHandler = props => {
     const { year } = useParams()
     const [data, setData] = useState(null)
-    const [url, setUrl] = useState('https://data.cityofnewyork.us/resource/qgea-i56i.json')
     const [complaints, setComplaints] = useState([])
     const [currentComplaint, setCurrentComplaint] = useState(null)
 
-    const apiCall = async () => {
-        const response = await axios.get(url)
-        setData(response.data)
-        setComplaints(response.data.map((complaint) => {
-            const latLong = [ parseFloat(complaint.latitude), parseFloat(complaint.longitude)]
-            const customPin = {
-                width: '10px',
-                height: '10px'
-            }
-         const imageSelector = () => {switch (complaint.law_cat_cd) {
-                     case 'FELONY' :
-                        return Felony
-                     case 'MISDEMEANOR' :
-                        return Misdemeanor
-                     case 'VIOLATION' :
-                        return Violation
-                }
-            }
-
-            return (
-                (complaint.latitude)
-                ? (<Marker key={complaint.cmplnt_num} latitude={latLong[0]} longitude={latLong[1]}>
-                    <div style={customPin}
-                        onClick={() => {setCurrentComplaint(complaint)}}
-                        onTouch={() => {setCurrentComplaint(complaint)}}
-                        >
-                        <img style={{width: '10px',height: '10px'}} src={imageSelector(complaint.law_cat_cd)} alt='pin'/>
-                    </div>
-                </Marker>)
-                : null)
-        }))
-    }
-
     useEffect(() => {
-            setCurrentComplaint(null)
-        setUrl(`https://data.cityofnewyork.us/resource/qgea-i56i.json?$where=cmplnt_fr_dt%20between%20%27${year}-01-01T00:00:00%27%20and%20%27${year}-12-31T23:59:00%27`)
+        const apiCall = async () => {
+            const url = `https://data.cityofnewyork.us/resource/qgea-i56i.json?$where=cmplnt_fr_dt%20between%20%27${year}-01-01T00:00:00%27%20and%20%27${year}-12-31T23:59:00%27`
+            const response = await axios.get(url)
+            setData(response.data)
+            setComplaints(response.data.map((complaint) => {
+                const latLong = [ parseFloat(complaint.latitude), parseFloat(complaint.longitude)]
+                const customPin = {
+                    width: '10px',
+                    height: '10px'
+                }
+             const imageSelector = () => {switch (complaint.law_cat_cd) {
+                         case 'FELONY' :
+                            return Felony
+                         case 'MISDEMEANOR' :
+                            return Misdemeanor
+                         case 'VIOLATION' :
+                            return Violation
+                         default :
+                            return undefined
+                    }
+                }
+
+                return (
+                    (complaint.latitude)
+                    ? (<Marker key={complaint.cmplnt_num} latitude={latLong[0]} longitude={latLong[1]}>
+                        <div style={customPin}
+                            onClick={() => {setCurrentComplaint(complaint)}}
+                            onTouch={() => {setCurrentComplaint(complaint)}}
+                            >
+                            <img style={{width: '10px',height: '10px'}} src={imageSelector(complaint.law_cat_cd)} alt='pin'/>
+                        </div>
+                    </Marker>)
+                    : null)
+            }))
+        }
+
+        setCurrentComplaint(null)
         apiCall()
     }, [year])
 
